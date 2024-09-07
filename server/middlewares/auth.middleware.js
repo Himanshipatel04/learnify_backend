efbadb8696e1010c79c97844e8765f116ad9e3ed
Himanshipatel04 <himanshipatel0409@gmail.com>
2024-09-07 15:26:58 +0530

    fixed getUser

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -8,21 +8,16 @@ export const verifyJWT = async (req, res, next) => {
       req.cookies?.accessToken ||
       req.body?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
-    console.log(token,".......");
-    
-    console.log("hello guys");
-    
+      // console.log(token, ".......");
+
     if (!token) {
-      console.log("Token not found");
+      // console.log("Token not found");
       return res
         .status(401)
         .json(new ApiError(401, "Unauthorized access to website!"));
     }
-  
 
     const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-    
 
     const user = await StudentModel.findOne({
       email: decodeToken.email,
@@ -38,8 +33,10 @@ export const verifyJWT = async (req, res, next) => {
   } catch (error) {
     console.error("Error fetching user:", error);
 
-    if (error.name === 'JsonWebTokenError') {
-      return res.status(401).json(new ApiError(401, "Invalid or expired token."));
+    if (error.name === "JsonWebTokenError") {
+      return res
+        .status(401)
+        .json(new ApiError(401, "Invalid or expired token."));
     }
 
     return res
